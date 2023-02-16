0922ee093328c89afbc8a2720452f6248402c2be
Amit maurya <118551644+Mech-Amit-2016@users.noreply.github.com>
2023-02-16 14:28:23 +0530

    Added Search  Queries

--- demo/src/main/java/com/blogspot/myproject/restcontrollers/RestApiController.java
+++ demo/src/main/java/com/blogspot/myproject/restcontrollers/RestApiController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RestApiController {
+public class        RestApiController {
     @GetMapping("/other")
 
     public String doTest() {
--- demo/src/main/java/com/blogspot/myproject/vsjmain/VsjDemoApplication.java
+++ demo/src/main/java/com/blogspot/myproject/vsjmain/VsjDemoApplication.java
@@ -3,10 +3,7 @@ package com.blogspot.myproject.vsjmain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,8 +16,17 @@ public class VsjDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(VsjDemoApplication.class, args);
         System.out.println("hello");
+//        Student list[]={new Student(1,"A","address1"),new Student(2,"B","Address2"),
+//                        new Student(3,"C","Address3"),new Student(3,"D","Address4"),
+//                        new Student(5,"D","Address5")
+//        };
     }
 
+    Student list[]={new Student(0,"A","address0"),new Student(1,"B","Address1"),
+            new Student(2,"C","Address2"),new Student(3,"D","Address3"),
+            new Student(4,"E","Address4"),new Student(5,"F","Address5"),
+            new Student(6,"G","Address6")
+    };
     //**************************************************************************
     @GetMapping("/Amit")
     public String myName() {
@@ -43,9 +49,9 @@ public class VsjDemoApplication {
 
     @GetMapping("/sub")
 
-        public String dosub(@RequestParam(value="a", defaultValue="0") int a, @RequestParam(value="b" ,defaultValue="0") int b) {
-            return String.format("%s - %s = %s",a,b,a-b);
-        }
+    public String dosub(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b) {
+        return String.format("%s - %s = %s", a, b, a - b);
+    }
 
     @GetMapping("/add")
 
@@ -65,6 +71,27 @@ public class VsjDemoApplication {
         return String.format("%s +  %s = %s", x, y, x + y);
     }
 
+    @GetMapping("/pathparam/{a}/{b}")
+    public @ResponseBody
+    String pathparam(@PathVariable Optional<String> a, @PathVariable Optional<String> b) {
+        if (!a.isPresent())
+            a = Optional.of("0");
+        if (!b.isPresent())
+            b = Optional.of("1");
+
+        return "Path " +  a.get() + ":" + b.get();
+    }
+
+    @GetMapping("/mypathparam/{a}/{b}")
+    public @ResponseBody
+    String mypathparam(@PathVariable Optional<String> a, @PathVariable Optional<String> b){
+        if(!a.isPresent())
+            a=Optional.of("0");
+        if(!b.isPresent())
+            b=Optional.of("1");
+        return "path" + a.get() + ":" +b.get() + ":"+ (Integer.parseInt(a.get())+Integer.parseInt(b.get()));
+    }
+
     @GetMapping("/book")
     public ResponseEntity<Book> book() {
         try {
@@ -74,6 +101,28 @@ public class VsjDemoApplication {
 
         } catch (Exception ex) {
             return ResponseEntity.notFound().build();//Return Not Found
+        }
+    }
+
+    @GetMapping("/student")
+    public ResponseEntity<Student> getStudent(@RequestParam(value = "rollno", defaultValue = "0") int rollno){
+        try{
+//            Student student=new Student(12,"amit","varanasi");
+            return ResponseEntity.ok(list[rollno]);
+        }
+        catch(Exception ex){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/allstudent")
+    public ResponseEntity<Student[]> getStudent(){
+        try{
+//            Student student=new Student(12,"amit","varanasi");
+            return ResponseEntity.ok(list);
+        }
+        catch(Exception ex){
+            return ResponseEntity.notFound().build();
         }
     }
 }
